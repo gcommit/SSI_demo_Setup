63490489c65de4c0aa10fa1b02a83f23254fb9a1
gcommit <mariusgebert@gmx.de>
2022-01-26 10:34:31 +0100

    not working somehow

--- start_alice.sh
+++ start_alice.sh
@@ -2,4 +2,4 @@
 # HOW TO START ALL COMPONENTS
 
 # START ALICE AGENT
-LEDGER_URL=http://$IP:9000 POSTGRES="true" $HOME/ssi/aries-cloudagent-python/demo/run_demo alice
+LEDGER_URL=http://192.168.65.3:9000 POSTGRES="true" $HOME/ssi/aries-cloudagent-python/demo/run_demo alice
--- start_faber.sh
+++ start_faber.sh
@@ -2,4 +2,4 @@
 # HOW TO START ALL COMPONENTS
 
 # START FABER AGENT
-LEDGER_URL=http://$IP:9000 POSTGRES="true" PUBLIC_TAILS_SERVER=http://192.168.65.3:6543 $HOME/ssi/aries-cloudagent-python/demo//run_demo faber --revocation
+LEDGER_URL=http://192.168.65.3:9000 POSTGRES="true" PUBLIC_TAILS_SERVER=http://192.168.65.3:6543 $HOME/ssi/aries-cloudagent-python/demo//run_demo faber --revocation
